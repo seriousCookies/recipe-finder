a9b5b63e9f8ef4a428502d2e13c59d273bdc7b86
laSarique <na.wei95@hotmail.com>
2020-11-07 13:22:51 +0100

    recipe fetched, flip works, hover works

--- recipefinder/src/components/RecipeItem.js
+++ recipefinder/src/components/RecipeItem.js
@@ -7,23 +7,25 @@ const [recipe, setRecipe]=useState();
 
 const fetchRecipes= async e => {
 e.preventDefault();
+console.log(e.target.tagName ==='LI');
+e.target.closest(".card").classList.toggle('is-flipped');
 await fetchData(id,getRecipe, setRecipe)
 }
 
   return (
-    <>
+    <div className= 'item-container scene' >
     <button
     onClick={fetchRecipes}
-    className="gallery btn">
-            <img className="gallery_img" src={image} alt={title}/>
-            <div className="gallery_desc">{title}</div>
-            {recipe ?
+    className="item btn card">
+            <img className="item_img card__face card__face--front" src={image} alt={title}/>
+            <div className="item_desc overlay card__face card__face--front">{title}</div>
+            {recipe && recipe.length>1 ?
               <RecipeSteps
               recipe={recipe}
               />
             :''}
     </button>
-    </>
+    </div>
   );
 }
 export default RecipeItem;
--- recipefinder/src/components/RecipeSteps.js
+++ recipefinder/src/components/RecipeSteps.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 
 const RecipeSteps = ({recipe}) => {
-
   return (
+    <div className="item_recipe card__face card__face--back overlay">
       <ol>
-    {recipe? recipe.map(t => 
-    <li key={t}>
+      {recipe && recipe.length>1 ? recipe.map(t => 
+      <li key={t}>
         {t}
-    </li>):''}
+      </li>):''}
       </ol>
-
+    </div>
   );
 }
 export default RecipeSteps;
