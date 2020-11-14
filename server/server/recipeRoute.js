b1953759e6a8db9321ac429cb6144e68d0989dd4
laSarique <na.wei95@hotmail.com>
2020-11-14 15:48:37 +0100

    fixing up backend

--- server/server/recipeRoute.js
+++ server/server/recipeRoute.js
@@ -10,30 +10,29 @@ const headers = {
 }
 
 router.get('/:id', (req, res) => {
-  const recipe1 = `[
-    "Marinate strips of flank steak in a drizzle of EVOO, 2 smashed garlic cloves, juice of 1 lime (or lemon), 4 tbs. chopped cilantro and a dash of the seasonings for an hour or two.",
-    "If desired, brush jalapenos with a little oil and roast in 400F preheated oven for 30-40 minutes, turning over once halfway through.",
-    "In a sauce pan, heat the enchilada sauce until boiling then add the beans. Stir for another couple of minutes and turn off heat.",
-    "In a medium-heated skillet, add a little EVOO and saut the beef strips along with the minced garlic and the julienned onion and bell pepper(s).",
-    "Stack the tortillas on top of each other and roll as tight as you can without tearing it. Now, using a very sharp knife, thinly slice the roll from one end to the other creating fettuccine-like strands. Loosen strands and coat with EVOO and a dash of the seasonings.",
-    "Spread tortilla strands out on individual serving platters, top each with enchilada/bean mixture, salsa and beef/veggie mixture.",
-    "Garnish with remaining chopped cilantro, a lime (or lemon) wedge and a roasted jalapeno pepper.",
-    "Another optional item you can add is shredded cheese. Before garnishing, top with shredded cheese and pop the plate in the microwave for a minute or until cheese has fully melted."
-    ]`;
-    res.json(JSON.parse(recipe1));
-// const id = req.params.id;
-// const url =`${baseURL}${id}/analyzedInstructions?${key}`;
-// console.log(url);
-//   fetch(url, { method: 'GET', headers: headers})
-//   .then((res) =>  res.json())
-//   .then((json) => {
-//     const data = json[0].steps;
-//     const filteredData = data.map(x=>x.step)
-//     res.json(filteredData);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.json({ error: err.message });
-//   });
+  // const recipe1 = `[
+  //   "Marinate strips of flank steak in a drizzle of EVOO, 2 smashed garlic cloves, juice of 1 lime (or lemon), 4 tbs. chopped cilantro and a dash of the seasonings for an hour or two.",
+  //   "If desired, brush jalapenos with a little oil and roast in 400F preheated oven for 30-40 minutes, turning over once halfway through.",
+  //   "In a sauce pan, heat the enchilada sauce until boiling then add the beans. Stir for another couple of minutes and turn off heat.",
+  //   "In a medium-heated skillet, add a little EVOO and saut the beef strips along with the minced garlic and the julienned onion and bell pepper(s).",
+  //   "Stack the tortillas on top of each other and roll as tight as you can without tearing it. Now, using a very sharp knife, thinly slice the roll from one end to the other creating fettuccine-like strands. Loosen strands and coat with EVOO and a dash of the seasonings.",
+  //   "Spread tortilla strands out on individual serving platters, top each with enchilada/bean mixture, salsa and beef/veggie mixture.",
+  //   "Garnish with remaining chopped cilantro, a lime (or lemon) wedge and a roasted jalapeno pepper.",
+  //   "Another optional item you can add is shredded cheese. Before garnishing, top with shredded cheese and pop the plate in the microwave for a minute or until cheese has fully melted."
+  //   ]`;
+  //   res.json(JSON.parse(recipe1));
+const id = req.params.id;
+const url =`${baseURL}${id}/analyzedInstructions?${key}`;
+  fetch(url, { method: 'GET', headers: headers})
+  .then((res) =>  res.json())
+  .then((json) => {
+    const data = json[0].steps;
+    const filteredData = data.map(x=>x.step)
+    res.json(filteredData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ error: err.message });
+  });
 });
 module.exports = router;
--- server/server/recipelistRoute.js
+++ server/server/recipelistRoute.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const express = require('express');
 const router = express.Router();
+const url = require('url');
 
 global.fetch = fetch;
 const key =`apiKey=${process.env.ACCESS_KEY}`
@@ -8,29 +9,32 @@ const baseURL = `https://api.spoonacular.com/recipes/complexSearch?${key}`;
 const headers = {
   "Content-Type": "application/json",
 }
+//https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2
+//&query=pasta&intolerances=peanut&gluten&number=2&offset=5
 const paramsParser = req => {
-parameters = req.params;
-  const paramsArr= []
-  for (const [key, value] of Object.entries(parameters)) {
-  value !== 'null' ? paramsArr.push(`&${key}=${value}`): null;
+
+const queries = req.params;
+  const queryArr= []
+  for (const [key, value] of Object.entries(queries)) {
+  value !== 'null' ? queryArr.push(`&${key}=${value}`): null;
   }
-  const paramString = paramsArr.join('');
-  return paramString;
+  const queryString = queryArr.join('');
+  return queryString;
 }
 
-router.get('/:query/:diet/:intolerances', (req, res) => {
-  const choco = "[{\"id\":654959,\"title\":\"Pasta With Tuna\",\"image\":\"https://spoonacular.com/recipeImages/654959-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":511728,\"title\":\"Pasta Margherita\",\"image\":\"https://spoonacular.com/recipeImages/511728-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654812,\"title\":\"Pasta and Seafood\",\"image\":\"https://spoonacular.com/recipeImages/654812-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654857,\"title\":\"Pasta On The Border\",\"image\":\"https://spoonacular.com/recipeImages/654857-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654883,\"title\":\"Pasta Vegetable Soup\",\"image\":\"https://spoonacular.com/recipeImages/654883-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654928,\"title\":\"Pasta With Italian Sausage\",\"image\":\"https://spoonacular.com/recipeImages/654928-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654926,\"title\":\"Pasta With Gorgonzola Sauce\",\"image\":\"https://spoonacular.com/recipeImages/654926-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654944,\"title\":\"Pasta With Salmon Cream Sauce\",\"image\":\"https://spoonacular.com/recipeImages/654944-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654905,\"title\":\"Pasta With Chickpeas and Kale\",\"image\":\"https://spoonacular.com/recipeImages/654905-312x231.jpg\",\"imageType\":\"jpg\"},{\"id\":654901,\"title\":\"Pasta With Chicken and Broccoli\",\"image\":\"https://spoonacular.com/recipeImages/654901-312x231.jpg\",\"imageType\":\"jpg\"}]";
-  res.json(JSON.parse(choco));
-  // const p = paramsParser(req);
-  // const url =`${baseURL}${p}`;
-  // fetch(url, { method: 'GET', headers: headers})
-  // .then((res) =>  res.json())
-  // .then((json) => {
-  //   res.json(json.results);
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  //   res.json({ error: err.message });
-  // });
+router.get('/:query', (req, res) => {
+  // const choco = `{"results":[{"id":633711,"title":"Baked Penne","image":"https://spoonacular.com/recipeImages/633711-312x231.jpg","imageType":"jpg"},{"id":662669,"title":"Swiss Chard Linguine","image":"https://spoonacular.com/recipeImages/662669-312x231.jpg","imageType":"jpg"},{"id":644885,"title":"Gluten Free Vegan Gnocchi","image":"https://spoonacular.com/recipeImages/644885-312x231.jpg","imageType":"jpg"},{"id":1095743,"title":"Coconut Curry Ramen Noodles","image":"https://spoonacular.com/recipeImages/1095743-312x231.jpg","imageType":"jpg"},{"id":631732,"title":"Caponata Style Celery Spaghetti","image":"https://spoonacular.com/recipeImages/631732-312x231.png","imageType":"png"},{"id":637440,"title":"Chapchae (Korean Stir-Fried Noodles)","image":"https://spoonacular.com/recipeImages/637440-312x231.jpg","imageType":"jpg"},{"id":662222,"title":"Summer Garlic Mushrooms and Mostaccioli","image":"https://spoonacular.com/recipeImages/662222-312x231.jpg","imageType":"jpg"},{"id":665527,"title":"Yellow Squash Noodles with Tomato Basil Sauce","image":"https://spoonacular.com/recipeImages/665527-312x231.jpg","imageType":"jpg"},{"id":1095750,"title":"Whole Wheat Spaghetti with Basil Avocado Pesto","image":"https://spoonacular.com/recipeImages/1095750-312x231.jpg","imageType":"jpg"},{"id":654009,"title":"Orecchiette With Sun-Dried and Fresh Cherry Tomatoes","image":"https://spoonacular.com/recipeImages/654009-312x231.jpg","imageType":"jpg"}],"offset":3,"number":10,"totalResults":14}`
+  // res.json(JSON.parse(choco));
+  const p = paramsParser(req);
+  const url =`${baseURL}${p}`;
+  fetch(url, { method: 'GET', headers: headers})
+  .then((res) =>  res.json())
+  .then((json) => {
+    res.json(json);
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({ error: err.message });
+  });
 });
 module.exports = router;
